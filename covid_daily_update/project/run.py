c0747ddcd1e045bd4b11776a7c20d905513928cc
Diogo Valentim dos Santos <diogovalentte10@gmail.com>
2021-09-06 13:51:44 -0300

    Update covid_daily_update

--- covid_daily_update/project/run.py
+++ covid_daily_update/project/run.py
@@ -3,14 +3,14 @@ import sqlite3
 
 # Add "src" to folders library path
 sys.path.append("/opt/airflow/project/src")
-from data.create_database import create_database_and_tables
 from data.populate_database import add_data_in_covid_states_table, add_data_in_brazil_covid_table
+from data.create_database import create_database_and_tables
 
 # Path to the database file
 PATH = "/opt/airflow/project/data/database/database.db"
 
 # Create connection and cursor with the database
-connection = sqlite3.connect(PATH)
+connection = sqlite3.connect(PATH)  # Connect with database or create it
 cursor = connection.cursor()
 
 # Execute functions
