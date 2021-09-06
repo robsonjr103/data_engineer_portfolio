7e39f06784d6ceb6b6645dcacd6d7bdd2c766e37
root <root@pop-os.localdomain>
2021-09-06 11:59:24 -0300

    Update covid_daily_update project

--- covid_daily_update/run.sh
+++ covid_daily_update/run.sh
@@ -2,10 +2,11 @@
 # This script set the environment within the container.
 
 # Update pip
-docker exec airflow_airflow-webserver_1 python3 -m pip install --upgrade pip
+#docker exec covid_daily_update_airflow-webserver_1 python3 -m pip install --upgrade pip
 
 # Installs the required libraries in the container environment
-docker exec airflow_airflow-webserver_1 pip install -r /opt/airflow/project/requiriments.txt
+#docker exec covid_daily_update_airflow-webserver_1 pip install -r /opt/airflow/project/requiriments.txt
 
 # Runs the script that creates the database and its tables, if they do not stretch
-docker exec airflow_airflow-webserver_1 python3 /opt/airflow/project/run.py
+docker exec covid_daily_update_airflow-webserver_1 python3 /opt/airflow/project/run.py
+
